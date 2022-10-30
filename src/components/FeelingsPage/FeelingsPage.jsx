ed96404028c860b3d86eb0bc6956b141f4c802e0
Astrid Pulse <astridcpulse@gmail.com>
2022-10-30 15:52:19 -0500

    impliment MUI with slider and button on Feelings page. Refactor to impliment useState to store local state in Feelings page

--- src/components/FeelingsPage/FeelingsPage.jsx
+++ src/components/FeelingsPage/FeelingsPage.jsx
@@ -1,21 +1,54 @@
 import { useDispatch } from 'react-redux';
 import {useHistory} from 'react-router-dom';
+import { useState } from 'react';
+import { Button, Input, Slider, Box } from '@mui/material';
 
 
 function FeelingsPage(){
+    const [feeling, setFeeling] = useState(0);
     const dispatch = useDispatch();
     const history = useHistory();
 
-
     function handleChange(evt){
         evt.preventDefault();
-        let ranking = document.forms['feelingForm']['input'].value;
+        // let ranking = document.forms['feelingForm']['input'].value;
 
         dispatch({
             type:'ADD_FEELING',
-            payload: ranking
+            payload: feeling
         });
         history.push('/understandingpage');
+    }
+
+    const marks=[
+        {
+            value: 0,
+            label: 'I AM IN DESPAIR'
+        },
+        {
+            value: 1,
+            label: 'Not Good'
+        },
+        {
+            value: 2,
+            label: 'Little Frustrated'
+        },
+        {
+            value: 3,
+            label: 'OK'
+        },
+        {
+            value: 4,
+            label: 'Good'
+        },
+        {
+            value: 5,
+            label: 'Im Killin It! 😎'
+        },
+    ];
+    
+    const handleSlider = (evt) => {
+        setFeeling(evt.target.value);
     }
 
     return(
@@ -24,20 +57,38 @@ function FeelingsPage(){
                 <h1> How are you feeling today?</h1>
             </section>
             <form name ="feelingForm" onSubmit={handleChange}>
-                <input 
+                {/* <Input 
+                    // color="info"
+                    inputProps={{ min: 0, max: 5 }}
+                    placeholder="rating?"
                     name="input"
                     type="number" 
-                    min="0" 
                     max="5"
                     required
+                    variant="outlined"
                 > 
-                </input>
+                </Input> */}
 
-                <button 
+                <Box sx={{ m: 15 }}>
+                <Slider
+                    defaultValue={0}
+                    valueLabelDisplay="auto"
+                    step={1}
+                    marks={marks}
+                    min={0}
+                    max={5}
+                    onChange={handleSlider}
+                />
+                </Box>
+
+                <Button 
                     type="submit"
+                    variant="contained"
+                    size='large'
+                    sx={{ m: 3 }}
                 >
                     Next
-                </button>
+                </Button>
 
             </form>
         </>
